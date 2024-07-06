7263e999b6a0dc87a7d53c0d0af45f278eb18188
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2024-07-06 16:53:26 +0530

    Add About superQuality section

--- src/sections/SuperQuilty.jsx
+++ src/sections/SuperQuilty.jsx
@@ -1,12 +1,36 @@
-
+import { shoe8 } from "../assets/images";
+import Button from "../Components/Button";
 
 const SuperQuilty = () => {
   return (
-    <div>
-        SuperQuilty
-      
-    </div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div>
+        <h1 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+          We Provide your
+          <span className="text-coral-red "> Super </span>
+          <span className="text-coral-red ">Quality</span> Shoes
+        </h1>
 
-export default SuperQuilty
+        <p className="info-text mt-4 lg:max-w-lg">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="info-text mt-6 lg:max-w-lg">
+          Our dedication to detail and excellence ensures your
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" iconUrl={""} />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center flex-1">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuilty;
